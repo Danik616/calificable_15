8fd2f8cba4899547422f2e9dca363b65ae874235
Danik616 <ddanielvargas2011@gmail.com>
2023-07-17 08:58:43 -0500

    Se hace la creacion inicial del proyecto

---
+++ src/main/java/com/entregable/u15/U15Application.java
@@ -0,0 +1,13 @@
+package com.entregable.u15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/entregable/u15/U15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entregable.u15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
